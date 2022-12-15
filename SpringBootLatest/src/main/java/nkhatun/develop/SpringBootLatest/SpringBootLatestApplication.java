fda1106706092894bc9d4ea795c24283a5878904
nkhatun <nknajada@gmail.com>
2022-12-15 16:12:36 +0100

    Initial commit - spring boot latest
    Base version of spring boot latest version's repository

---
+++ SpringBootLatest/src/main/java/nkhatun/develop/SpringBootLatest/SpringBootLatestApplication.java
@@ -0,0 +1,13 @@
+package nkhatun.develop.SpringBootLatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLatestApplication.class, args);
+	}
+
+}
